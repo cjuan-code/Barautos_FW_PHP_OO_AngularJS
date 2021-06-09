1cf12ab004862332c942840b166a17b7c6d4201c
cjuan-code <cjuaniestacio@gmail.com>
2021-06-09 03:24:05 +0200

    File cleaning

--- backend/model/apis/apis.js
+++
@@ -1,5 +0,0 @@
-const apiKeySL = 'AIzaSyBuOI26v_bbaHyJ5RpMKIWRJtBA-AcNv9I';
-const domainSL = 'ssg-ab.firebaseapp.com';
-const urlSL = 'https://ssg-ab.firebaseio.com';
-const idSL = 'ssg-ab';
-const pjSL = '923348136925'; 
